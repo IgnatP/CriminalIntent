027d83d9edb1c069070ab4fddf82ba8badc7ab83
IgnatP <ipomazkov@icloud.com>
2016-05-18 11:26:53 +0300

    test commit

--- app/src/main/java/com/ivpomazkov/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/ivpomazkov/criminalintent/CrimeFragment.java
@@ -189,6 +189,7 @@ public class CrimeFragment extends Fragment {
         mPhotoButton = (ImageButton) v.findViewById(R.id.crime_camera);
         mPhotoView = (ImageView) v.findViewById(R.id.crime_photo);
         Log.d("info", "observer");
+        //comments
         ViewTreeObserver observer = mPhotoView.getViewTreeObserver();
         observer.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
             @Override
